7a362e456b1054a5ec3616a7ef2f2f95be9b5017
Andreas Janning <andreas.janning@qaware.de>
2018-03-15 13:24:50 +0100

    added sorting by timestamp (newest first) to list of builds

--- proving-ground-webapp/src/main/java/de/qaware/pg/mvc/ProjectsController.java
+++ proving-ground-webapp/src/main/java/de/qaware/pg/mvc/ProjectsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Controller
@@ -56,6 +57,7 @@ public class ProjectsController {
         ProjectView project = persistence.findProjectByName(projectName);
         BranchView branch = persistence.findBranchByName(project.getId(), branchName);
         List<BuildView> builds = persistence.listBuildsForBranch(branch.getId());
+        builds.sort(Comparator.comparing(BuildView::getStartTime, Comparator.reverseOrder()));
         ModelAndView modelAndView = new ModelAndView("builds");
         modelAndView.addObject("project", project);
         modelAndView.addObject("branch", branch);
