d8417a8c4eef2fa8d352be77cb5e84391b949c54
Andreas Janning <andreas.janning@qaware.de>
2018-08-13 14:06:29 +0200

    limit the heap memory of the proving-ground process to 256mb

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec java -jar /application/proving-ground-webapp.jar --spring.config.name=application-cap
+exec java -jar /application/proving-ground-webapp.jar --spring.config.name=application-cap -Xmx 256m
